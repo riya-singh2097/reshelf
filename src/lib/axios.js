346d2225fa3ab053606abfb9c1eda73a838ac4af
riya singh <singhriya2531@gmail.com>
2026-04-10 14:51:02 +0530

    added backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL|| "http://localhost:3000/api",
 });
 
 // Helper function to ensure Firebase is ready before the interceptor runs
